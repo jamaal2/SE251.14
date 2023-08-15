c482df01d4b585217ec1ac8854d2059c8110e123
jamaal2 <jdublinjr@email.neit.edu>
2023-08-15 13:27:26 -0400

    pong

---
+++ w1/js/Player.js
@@ -0,0 +1,8 @@
+class Player {
+    constructor(){
+        this.name = 'Player' // player name
+        this.score = 0 // player score
+        this.highscore = 0 //player highscore
+        this.pad = new Box() // player pad
+    }
+}
